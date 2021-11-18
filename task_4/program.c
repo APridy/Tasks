7f10dd7fcacf8ce5d425e33156756bc11e5fd9ea
APridy <anton.pridybailo@gmail.com>
2021-11-18 22:23:57 +0300

    Add working pipe

--- task_4/program.c
+++ task_4/program.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h> 
 
+int fd[2];
 
 void process_A() {
+	close(fd[0]);
+	
 	printf("Process A\n");
+	int value;
+	while(1) {
+		scanf("%d", &value);
+		write(fd[1], &value, sizeof(int));
+		//close(fd[1]);
+	}
 }
 
 void process_B() {
+	close(fd[1]);
+
         printf("Process B\n");
+	int value;
+	while(1) {
+		read(fd[0], &value, sizeof(int));
+		//close(fd[0]);
+		printf("Got from A: %d\n", value*value);
+	}
 }
 
 void process_C() {
@@ -16,24 +33,29 @@ void process_C() {
 }
 
 int create_process(void (*fun_ptr)() ) {
-
 	int pid = fork();
-	if (pid != 0) {	
+
+	if (pid != 0) {			
 		fun_ptr();
 		exit(0);
 	}
 
 	return pid;
-
 }
 
 int main() {
-
 	int A_pid, B_pid, C_pid;		
+
+	if (pipe(fd) == -1) {
+
+		printf("An error occured when opening the pipe\n");
+		return 1;
+	}
 
 	create_process(&process_A);
 	create_process(&process_B);
 	create_process(&process_C);
 
+	return 0;
 }
 
