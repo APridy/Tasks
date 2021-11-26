8d67b2a0ed8af2e56637647aa69136a92305ce87
APridy <anton.pridybailo@gmail.com>
2021-11-26 12:23:40 +0300

    Add stopthread mechanic

--- task_4/program.c
+++ task_4/program.c
@@ -15,6 +15,9 @@
 #define PID_A (*(struct shared_memory*)shmem).pid_A
 #define PID_B (*(struct shared_memory*)shmem).pid_B
 #define PID_C (*(struct shared_memory*)shmem).pid_C
+#define STOP_PROCESS (*(struct shared_memory*)shmem).stop_process
+
+volatile int stop = false;
 
 int fd[2];
 struct shared_memory {
@@ -23,15 +26,28 @@ struct shared_memory {
 	pid_t pid_A;
 	pid_t pid_B;
 	pid_t pid_C;
+	volatile bool stop_process;
 };
 void* shmem;
+
+void handle_sigterm_A() {
+	stop = true;
+	printf("Sigterm A\n");
+}
 
 void process_A() {
 	close(fd[0]);
 	int value;
 	char c;
 
-	while (1) {
+	struct sigaction sa = { 0 }; // Process A SIGTERM initialization
+        sa.sa_flags = SA_RESTART;
+        sa.sa_handler = &handle_sigterm_A;
+        sigaction(SIGTERM, &sa, NULL);
+      
+	//signal(SIGTERM, handle_sigterm_A); // Process A SIGTERM initialization
+
+	while (!stop) {
 		if (scanf("%d", &value) == 1) { 
 			write(fd[1], &value, sizeof(int));
 		}
@@ -40,37 +56,52 @@ void process_A() {
 			while((c = getchar()) != '\n' && c != EOF);
 		};
 	}
+	close(fd[1]);
+	printf("A process end\n");
 }
 
 void handle_sigusr1_B() {
-	kill(PID_C, SIGKILL);
-        kill(PID_A, SIGKILL);
-        kill(PID_B, SIGKILL);
+	//kill(PID_C, SIGKILL);
+	STOP_PROCESS = true;
+        kill(PID_A, SIGTERM);
+        kill(PID_B, SIGTERM);
+}
+
+void handle_sigterm_B() {
+	printf("Sigterm B \n");
+	stop = true;
 }
 
 void process_B() {
 	close(fd[1]);
 	int value;
 	bool val_changed = true;
-
 	struct sigaction sa = { 0 }; // Process B SIGUSR1 initialization
 	sa.sa_flags = SA_RESTART;
 	sa.sa_handler = &handle_sigusr1_B;
 	sigaction(SIGUSR1, &sa, NULL);
 
-	while (1) {
+	struct sigaction sa1 = { 0 }; // Process B SIGTERM initialization
+        sa1.sa_flags = SA_RESTART;
+        sa1.sa_handler = &handle_sigterm_B;
+        sigaction(SIGTERM, &sa1, NULL);
+		
+	while (!stop) {
 		read(fd[0], &value, sizeof(int));
+		printf("Read value in process B\n");
 		value *= value;
 		memcpy(shmem, &value, sizeof(value));
 		memcpy(shmem + sizeof(int), &val_changed, sizeof(bool));
 	}
+	close(fd[0]);
+	printf("B process end\n");
 }
 
 bool value_changed = false;
 
 void* process_C_1() {
 	int value = 0; 
-	while (1) {
+	while (!STOP_PROCESS) {
 		if (SHMEM_VALUE_CHANGED) {
 			SHMEM_VALUE_CHANGED = false;
 			value_changed = true;
@@ -81,7 +112,7 @@ void* process_C_1() {
 }
 
 void* process_C_2() {
-	while (1) {
+	while (!STOP_PROCESS) {
                 if (value_changed) {
 			value_changed = false;
 			printf("Value = %d\n", SHMEM_VALUE);
@@ -119,7 +150,7 @@ int create_process(void (*fun_ptr)()) {
 }
 
 int main() {
-	struct shared_memory shmem_init = { .value = 0, .val_changed = false}; // Shared memory initialization
+	struct shared_memory shmem_init = { .value = 0, .val_changed = false, .stop_process = false}; // Shared memory initialization
         int shmem_value = 0;
         int protection = PROT_READ | PROT_WRITE;
         int visibility = MAP_SHARED | MAP_ANONYMOUS;
