fe8b6e7ac461352c336dba0a4ddf2939e06baf44
APridy <anton.pridybailo@gmail.com>
2021-12-04 00:00:05 +0300

    Add data union. Add integer send/recieve

--- task_5/client.c
+++ task_5/client.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
+#define QUEUE_KEY 252525
 #define NUM_OF_DATA_TYPES 3
 
 struct mystruct {
@@ -15,8 +22,29 @@ union data {
 	struct mystruct num3;
 };
 
-FILE *fp[NUM_OF_DATA_TYPES];
-char *filename[NUM_OF_DATA_TYPES] = {"int.txt", "array.txt", "struct.txt"};
+struct message {
+	long mtype;
+	uint8_t msg[sizeof(union data)];
+};
+
+void clear_stdin() {
+        char c;
+        while((c = getchar()) != '\n' && c != EOF);
+}
+
+union data get_integer() {
+	union data value;
+	while(scanf("%d", &(value.num)) != 1) {
+		printf("Invalid input. Enter integer value\n");
+		clear_stdin();
+	}
+	return value;
+}
+
+
+//FILE *fp[NUM_OF_DATA_TYPES];
+char *data_type[NUM_OF_DATA_TYPES] = {"Integer", "Char array", "Structure with 3 integers"};
+union data (*get_data[NUM_OF_DATA_TYPES])() = {&get_integer, NULL, NULL};
 
 int main(int argc, char **argv) {
 
@@ -32,4 +60,31 @@ int main(int argc, char **argv) {
                 };
         };
 
+	int msgid;
+	key_t msgkey = QUEUE_KEY;
+	msgid = msgget(msgkey, IPC_CREAT | 0666/*| IPC_EXCL*/);
+	printf("Message queue id: %d\n",msgid);
+
+	printf("Choose what data type to send:\n");
+	for (int i = 0; i < NUM_OF_DATA_TYPES; i++) {
+		printf("%d: %s\n", i+1, data_type[i]);
+	} 
+	
+	int choice = 0;
+	while((scanf("%d", &choice) != 1) || ((choice < 1) || (choice > NUM_OF_DATA_TYPES))) {
+                printf("Invalid input.\nEnter integer value in range  1 - %d\n", NUM_OF_DATA_TYPES);
+                clear_stdin();
+        }
+
+	struct message sbuf;
+	size_t buf_length;
+	sbuf.mtype = 1;
+	union data dat = get_data[0]();
+	printf("%d\n",dat.num);
+	memcpy(sbuf.msg, &dat, sizeof(union data));
+	buf_length = strlen(sbuf.msg) + 1 ;
+	if (msgsnd(msgid, &sbuf, buf_length, IPC_NOWAIT) < 0) exit(1);
+	else printf("Message: \"%d\" Sent\n", *(int*)sbuf.msg);
+	
+	return 0;
 }
--- task_5/server.c
+++ task_5/server.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+#define QUEUE_KEY 252525
 #define NUM_OF_DATA_TYPES 3
 
 struct mystruct {
@@ -17,6 +22,12 @@ union data {
         char arr[5];
         struct mystruct num3;
 };
+
+struct message{
+	long mtype;
+	uint8_t msg[sizeof(union data)];
+};
+
 
 FILE *fp[NUM_OF_DATA_TYPES];
 char *filename[NUM_OF_DATA_TYPES] = {"int.txt", "array.txt", "struct.txt"};
@@ -50,10 +61,41 @@ int main(int argc, char **argv) {
 	}
 	
 	int msgid;
-	key_t msgkey = 252525;
+	key_t msgkey = QUEUE_KEY;
 	msgid = msgget(msgkey, IPC_CREAT | 0666/*| IPC_EXCL*/);
 	printf("Message queue id: %d\n",msgid);
+	
+	struct msqid_ds qstatus;
 
+	if(msgctl(msgid,IPC_STAT,&qstatus)<0){
+		perror("msgctl failed");
+		exit(1);
+	}
+	printf("Real user id of the queue creator: %d\n",qstatus.msg_perm.cuid);
+	printf("Real group id of the queue creator: %d\n",qstatus.msg_perm.cgid);
+	printf("Effective user id of the queue creator: %d\n",qstatus.msg_perm.uid);
+	printf("Effective group id of the queue creator: %d\n",qstatus.msg_perm.gid);
+ 	printf("Permissions: %d\n",qstatus.msg_perm.mode);
+	printf("Message queue id: %d\n",msgid);
+	printf("%ld message(s) on queue\n",qstatus.msg_qnum);
+	printf("Current number of bytes on queue %ld\n",qstatus.msg_cbytes);
+	printf("Maximum number of bytes allowed on the queue%ld\n",qstatus.msg_qbytes);
+
+	printf("\nLast message sent by process :%3d at %s \n",qstatus.msg_lspid,ctime(& (qstatus.msg_stime)));
+	printf("Last message received by process :%3d at %s \n",qstatus.msg_lrpid,ctime(& (qstatus.msg_rtime)));
+	
+	struct message rbuf;
+	union data dat = {.num = 0};
+	int twelve = 33;
+	while(1) {
+		msgrcv(msgid, &rbuf, sizeof(struct message), 1, IPC_NOWAIT);
+		memcpy(&dat, &rbuf, sizeof(union data));
+		//dat.num = rbuf.msg[0];
+		printf("%d\n", dat.num);
+		printf("%s\n",dat.arr);
+		sleep(1);
+	}
+	
 	return 0;
-
+	
 }
