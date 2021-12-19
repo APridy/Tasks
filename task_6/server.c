2f7ce8d8d254fc0687d6d7b331bc25973e37a406
APridy <anton.pridybailo@gmail.com>
2021-12-19 20:08:16 +0300

    Add long options parsing

--- task_6/server.c
+++ task_6/server.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-int main() {
+int g_thread_mode = 0, g_process_mode = 0;
 
+const struct option long_options[] = {
+	{ "pthreads", no_argument, &g_thread_mode, 1 },
+	{ "process", no_argument, &g_process_mode, 1 },
+	{ NULL, 0, NULL, 0}
+};
+
+int parse_args(int argc, char **argv) {
+	char arg = 0;
+	while ((arg = getopt_long_only(argc, argv, "", long_options, NULL)) != -1) {
+		if(arg == '?') return -1;
+	};
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	if(parse_args(argc,argv)) return -1;
+	printf("Parsing succesful!\n");
+	if(g_thread_mode) printf("Thread\n");
+	if(g_process_mode) printf("Process\n");
+	return 0;
 }
