b1b21906be8829e2538ec8429df8bbb2e6f7044e
APridy <anton.pridybailo@gmail.com>
2021-12-21 15:15:24 +0300

    Add multiprocess mode

--- task_6/server.c
+++ task_6/server.c
@@ -13,6 +13,7 @@
 #define TCP_PORT 5665
 #define BUFF_SIZE 256
 
+int g_socket_id;
 int g_thread_mode = 0, g_process_mode = 0;
 
 const struct option long_options[] = {
@@ -21,27 +22,29 @@ const struct option long_options[] = {
 	{ NULL, 0, NULL, 0}
 };
 
-int create_connection(int socket_id) {
-	struct sockaddr_in client_info;
-	int connection_id;
-	int len = sizeof(client_info);
-	connection_id = accept(socket_id, (struct sockaddr*)&client_info, &len);
-	if (connection_id < 0) {
-		printf("Server accept error!: %s\n", strerror(errno));
-		close(socket_id);
-		return -1;
+int create_process(int (*fun_ptr)(int), int connection_id) {
+	int pid = fork();
+	switch (pid) {
+		case -1: {
+			printf("An error occured with creating process\n");
+			return -1;
+		} break;
+		case 0: {
+			fun_ptr(connection_id);
+			exit(0);
+		} break;
+		default: return pid;
 	}
+}
 
-	printf("Client connected to the server!\n");
-
+int create_connection(int connection_id) {
 	char command[BUFF_SIZE];
-
 	while(1) {
 		bzero(command, BUFF_SIZE);
 		read(connection_id, command, BUFF_SIZE);
 		printf("Command from client: %s\n", command);
 		if (strncmp("exit", command, 4) == 0) {
-			printf("Server Exit...\n");
+			printf("Connection aborted...\n");
 			break;
 		}
 
@@ -71,21 +74,6 @@ int create_connection(int socket_id) {
 	return 0;
 }
 
-int create_process(void (*fun_ptr)()) {
-	int pid = fork();
-	switch (pid) {
-		case -1: {
-			printf("An error occured with creating process\n");
-			return -1;
-		} break;
-		case 0: {
-			fun_ptr();
-			exit(0);
-		} break;
-		default: return pid;
-	}
-}
-
 int parse_args(int argc, char **argv) {
 	char arg = 0;
 	while ((arg = getopt_long_only(argc, argv, "", long_options, NULL)) != -1) {
@@ -95,15 +83,13 @@ int parse_args(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-	int socket_id;
 	struct sockaddr_in server_info;
 
 	if(parse_args(argc,argv)) return -1;
-	printf("Parsing succesful!\n");
 	//if(g_thread_mode) printf("Thread\n");
 	//if(g_process_mode) printf("Process\n");
 
-	if((socket_id = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	if((g_socket_id = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		printf("Error while creating socket!: %s\n", strerror(errno));
 		return -1;
 	}
@@ -112,21 +98,32 @@ int main(int argc, char **argv) {
 	server_info.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_info.sin_port = htons(TCP_PORT);
 
-	if (bind(socket_id, (struct sockaddr*)&server_info, sizeof(server_info))) {
+	if (bind(g_socket_id, (struct sockaddr*)&server_info, sizeof(server_info))) {
 		printf("Error while binding socket!: %s\n", strerror(errno));
-		close(socket_id);
+		close(g_socket_id);
 		return -1;
 	}
     
-	if (listen(socket_id, 5)) {
+	if (listen(g_socket_id, 5)) {
 		printf("Error while listening!: %s\n", strerror(errno));
-		close(socket_id);
+		close(g_socket_id);
 		return -1;
 	}
 
-	create_connection(socket_id);
-	
-	close(socket_id);
+	while(1) {
+		struct sockaddr_in client_info;
+		int connection_id;
+		int len = sizeof(client_info);
+		connection_id = accept(g_socket_id, (struct sockaddr*)&client_info, &len);
+		if (connection_id < 0) {
+			printf("Server accept error!: %s\n", strerror(errno));
+			close(g_socket_id);
+			return -1;
+		}
+		create_process(&create_connection, connection_id);
+		printf("Client connected to the server!\n");
+	}	
+	close(g_socket_id);
 	printf("Program execution ended\n");
 	return 0;
 }
