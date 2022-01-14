7cde7d777c7dd8d4a0ec087ec178bd08bf89d676
APridy <anton.pridybailo@gmail.com>
2022-01-14 22:57:37 +0300

    Add argument parsing

--- task_7/program.c
+++ task_7/program.c
@@ -1,3 +1,31 @@
-int main() {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <stdio.h>
+#include <unistd.h>
 
+unsigned int g_interval[2] = {0 , 0};
+
+int parse_args(int argc, char **argv) {
+	char arg = 0;
+	while ((arg = getopt(argc,argv,"b:e:")) != -1) {
+		switch (arg) {
+			case 'b':
+				g_interval[0] = atoi(optarg);
+				break;
+			case 'e':
+				g_interval[1] = atoi(optarg);
+				break;
+			case '?':
+				printf("Invalid argument!\n");
+				return -1;
+		};
+	};
+	return 0;
+
+}
+
+int main(int argc, char **argv) {
+	if(parse_args(argc, argv)) return -1;	
+	printf("%d\n%d\n", g_interval[0], g_interval[1]);
+	return 0;
 }
