71818ab2379f09812b4afb8e7e423d427c1307bb
APridy <anton.pridybailo@gmail.com>
2022-01-25 23:43:07 +0300

    Add audio transcoding to AC3(malfunctioning)

--- task_7/program.c
+++ task_7/program.c
@@ -5,6 +5,9 @@
 #include <libavutil/timestamp.h>
 #include <libswscale/swscale.h>
 #include <libavutil/opt.h>
+#include "libavutil/channel_layout.h"
+#include "libavutil/common.h"
+#include "libavutil/frame.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -45,12 +48,153 @@ typedef struct StreamingContext {
 	char *filename;
 } StreamingContext;
 
+static int select_sample_rate(const AVCodec *codec)
+{
+	const int *p;
+	int best_samplerate = 0;
+	if (!codec->supported_samplerates)
+		return 44100;
+	p = codec->supported_samplerates;
+	while (*p) {
+		best_samplerate = FFMAX(*p, best_samplerate);
+		p++;
+	}
+	return best_samplerate;
+}
+
+int create_streams(StreamingContext *input_media, StreamingContext *output_media) {
+	int ret;
+	for (int i = 0; i < input_media->avfc->nb_streams; i++) {
+		if (input_media->avfc->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+			input_media->video_avs = input_media->avfc->streams[i];
+			output_media->video_avs = avformat_new_stream(output_media->avfc, avcodec_find_decoder(input_media->avfc->streams[i]->codecpar->codec_id));;
+
+			if (!output_media->video_avs) {
+				printf("Failed allocating output stream\n");
+				return AVERROR_UNKNOWN;
+			}
+			ret = avcodec_parameters_copy(output_media->video_avs->codecpar, input_media->video_avs->codecpar);
+			if (ret < 0) {
+				printf("Error: failed to copy codec parameters from input to output stream codec parameters\n");
+				return ret; 
+			}
+		}
+		if (input_media->avfc->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+			input_media->audio_avs = input_media->avfc->streams[i];
+			output_media->audio_avs = avformat_new_stream(output_media->avfc, avcodec_find_decoder(input_media->avfc->streams[i]->codecpar->codec_id));;
+
+			if (!output_media->audio_avs) {
+				printf("Failed allocating output stream\n");
+				return AVERROR_UNKNOWN;
+			}
+			
+			ret = avcodec_parameters_copy(output_media->audio_avs->codecpar, input_media->audio_avs->codecpar);
+			if (ret < 0) {
+				printf("Error: failed to copy codec parameters from input to output stream codec parameters\n");
+				return ret;
+			}
+			output_media->audio_avs->time_base = input_media->audio_avs->time_base;
+			output_media->audio_avs->codecpar->codec_id = AV_CODEC_ID_AC3;
+		}
+	}
+}
+
+int prepare_audio_decoder(StreamingContext *input_media) {
+	input_media->audio_avc = avcodec_find_decoder(input_media->audio_avs->codecpar->codec_id);
+	input_media->audio_avcc = avcodec_alloc_context3(input_media->audio_avc);
+	avcodec_parameters_to_context(input_media->audio_avcc, input_media->audio_avs->codecpar);
+	avcodec_open2(input_media->audio_avcc, input_media->audio_avc, NULL);
+	return 0;
+}
+
+int prepare_audio_encoder(StreamingContext *output_media) {
+	output_media->audio_avc = avcodec_find_encoder(AV_CODEC_ID_AC3);
+	output_media->audio_avcc = avcodec_alloc_context3(output_media->audio_avc);
+
+	output_media->audio_avcc->time_base = output_media->audio_avs->time_base;
+	output_media->audio_avcc->sample_fmt = AV_SAMPLE_FMT_FLTP;
+	output_media->audio_avcc->channel_layout = AV_CH_LAYOUT_STEREO;
+	output_media->audio_avcc->channels = av_get_channel_layout_nb_channels(output_media->audio_avcc->channel_layout);
+	output_media->audio_avcc->sample_rate = 48000;
+	//output_media->audio_avcc->frame_size = 960; //960
+	//printf("%d\n", output_media->audio_avcc->frame_size);
+	avcodec_open2(output_media->audio_avcc, output_media->audio_avc, NULL);
+	return 0;
+}
+
+int encode_audio(StreamingContext *input_media, StreamingContext *output_media, AVFrame *frame, int stream_index, uint64_t *pts_start_from, uint64_t *dts_start_from) {
+	AVPacket *out_pkt = av_packet_alloc();
+	if (!out_pkt) {
+		//logging("could not allocate memory for output packet"); 
+		return -1;
+	}
+
+	output_media->audio_avcc->frame_size = frame->nb_samples;
+	int response = avcodec_send_frame(output_media->audio_avcc, frame);
+	//printf("Send Frame: %d, %s\n",response , av_err2str(response));
+	while (response >= 0) {
+		output_media->audio_avcc->frame_size = frame->nb_samples;
+		response = avcodec_receive_packet(output_media->audio_avcc, out_pkt);
+		//printf("Recieve Packet: %d, %s\n",response , av_err2str(response));
+
+		if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
+			break;
+		} else if (response < 0) {
+			return -1;
+		}
+
+		out_pkt->pts = av_rescale_q_rnd(out_pkt->pts - pts_start_from[out_pkt->stream_index], input_media->audio_avs->time_base, output_media->audio_avs->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+		out_pkt->dts = av_rescale_q_rnd(out_pkt->dts - dts_start_from[out_pkt->stream_index], input_media->audio_avs->time_base, output_media->audio_avs->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+		if (out_pkt->pts < 0) {
+			out_pkt->pts = 0;
+			}
+		if (out_pkt->dts < 0) {
+			out_pkt->dts = 0;
+		}
+
+		out_pkt->duration = (int)av_rescale_q((int64_t)out_pkt->duration, input_media->audio_avs->time_base, output_media->audio_avs->time_base);
+		out_pkt->pos = -1;
+
+		out_pkt->stream_index = stream_index;
+		
+		av_packet_rescale_ts(out_pkt, input_media->audio_avs->time_base, output_media->audio_avs->time_base);
+
+		response = av_interleaved_write_frame(output_media->avfc, out_pkt);
+		if (response != 0) { 
+			return -1;
+		}
+	}
+
+	av_packet_unref(out_pkt);
+	av_packet_free(&out_pkt);
+	return 0;
+}
+
+int transcode_audio(StreamingContext *input_media, StreamingContext *output_media, AVPacket *pkt, AVFrame *frame, uint64_t *pts_start_from, uint64_t *dts_start_from) {
+	int response = avcodec_send_packet(input_media->audio_avcc, pkt);
+	//printf("Send Packet: %d, %s\n",response , av_err2str(response));
+	while (response >= 0) {
+		response = avcodec_receive_frame(input_media->audio_avcc, frame);
+		//printf("Recieve Frame: %d, %s\n",response , av_err2str(response));
+		if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
+			break;
+		} else if (response < 0) {
+			return response;
+		}
+
+		if (response >= 0) {
+			if (encode_audio(input_media, output_media, frame, pkt->stream_index, pts_start_from, dts_start_from)) return -1;
+		}
+		av_frame_unref(frame);
+	}
+	return 0;
+}
+
 int cut_video(char* input_file, char* output_file, int interval_start, int interval_end) {
 	int ret;
-	
+
 	StreamingContext *input_media = (StreamingContext*) calloc(1, sizeof(StreamingContext));
 	input_media->filename = input_file;
-
 	input_media->avfc = avformat_alloc_context();
 
 	ret = avformat_open_input(&(input_media->avfc), input_media->filename, 0, 0);	
@@ -73,35 +217,15 @@ int cut_video(char* input_file, char* output_file, int interval_start, int inter
 		printf("Error: failed to create output context\n");
 		goto end;
 	}
-
-	for (int i = 0; i < input_media->avfc->nb_streams; i++) {
-		if (input_media->avfc->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-			output_media->video_avs = avformat_new_stream(output_media->avfc, avcodec_find_decoder(input_media->avfc->streams[i]->codecpar->codec_id));;
-			if (!output_media->video_avs) {
-				printf("Failed allocating output stream\n");
-				ret = AVERROR_UNKNOWN;
-				goto end;
-			}
-
-			if (avcodec_parameters_copy(output_media->video_avs->codecpar, input_media->avfc->streams[i]->codecpar) < 0) {
-				printf("Error: failed to copy codec parameters from input to output stream codec parameters\n");
-				goto end;
-			}
-		}
-		if (input_media->avfc->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-			output_media->audio_avs = avformat_new_stream(output_media->avfc, avcodec_find_decoder(input_media->avfc->streams[i]->codecpar->codec_id));;
-			if (!output_media->audio_avs) {
-				printf("Failed allocating output stream\n");
-				ret = AVERROR_UNKNOWN;
-				goto end;
-			}
-
-			if (avcodec_parameters_copy(output_media->audio_avs->codecpar, input_media->avfc->streams[i]->codecpar) < 0) {
-				printf("Error: failed to copy codec parameters from input to output stream codec parameters\n");
-				goto end;
-			}
-		}
+	
+	ret = create_streams(input_media, output_media);
+	if (ret < 0) {
+		printf("Error: failed to create output media streams\n");
+		goto end;
 	}
+
+	prepare_audio_decoder(input_media);
+	prepare_audio_encoder(output_media);
 
 	printf("------------------------------\n");	
 	av_dump_format(input_media->avfc, 0, input_file, 0);
@@ -135,6 +259,7 @@ int cut_video(char* input_file, char* output_file, int interval_start, int inter
 	memset(pts_start_from, 0, sizeof(int64_t) * input_media->avfc->nb_streams);
 
 	AVPacket *pkt = av_packet_alloc();
+	AVFrame *frame = av_frame_alloc();
 
 	while (1) {
 		AVStream *in_stream, *out_stream;
@@ -170,25 +295,31 @@ int cut_video(char* input_file, char* output_file, int interval_start, int inter
 			printf("pts_start_from: %s\n", av_ts2str(pts_start_from[pkt->stream_index]));
 		}
 
-		/* copy packet */
-		pkt->pts = av_rescale_q_rnd(pkt->pts - pts_start_from[pkt->stream_index], in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-		pkt->dts = av_rescale_q_rnd(pkt->dts - dts_start_from[pkt->stream_index], in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
-		if (pkt->pts < 0) {
-			pkt->pts = 0;
+		if (in_stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+			if (transcode_audio(input_media, output_media, pkt, frame, pts_start_from, dts_start_from)) return -1;
+			av_packet_unref(pkt);
 		}
-		if (pkt->dts < 0) {
-			pkt->dts = 0;
+		if (in_stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+			pkt->pts = av_rescale_q_rnd(pkt->pts - pts_start_from[pkt->stream_index], in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+			pkt->dts = av_rescale_q_rnd(pkt->dts - dts_start_from[pkt->stream_index], in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+			if (pkt->pts < 0) {
+				pkt->pts = 0;
+			}
+			if (pkt->dts < 0) {
+				pkt->dts = 0;
+			}
+
+			pkt->duration = (int)av_rescale_q((int64_t)pkt->duration, in_stream->time_base, out_stream->time_base);
+			pkt->pos = -1;
+
+			ret = av_interleaved_write_frame(output_media->avfc, pkt);
+			if (ret < 0) {
+				printf("Error muxing packet\n");
+				break;
+			}
+			av_packet_unref(pkt);
 		}
 
-		pkt->duration = (int)av_rescale_q((int64_t)pkt->duration, in_stream->time_base, out_stream->time_base);
-		pkt->pos = -1;
-
-		ret = av_interleaved_write_frame(output_media->avfc, pkt);
-		if (ret < 0) {
-			printf("Error muxing packet\n");
-			break;
-		}
-		av_packet_unref(pkt);	
 
 	}
 
@@ -198,7 +329,7 @@ int cut_video(char* input_file, char* output_file, int interval_start, int inter
 	av_write_trailer(output_media->avfc);
 
 end:
-	
+
 	avformat_close_input(&(input_media->avfc));
 
 	/* close output */
